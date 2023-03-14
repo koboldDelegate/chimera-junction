9fbefb02e6d3a9326ec4f69a74317d491740484c
koboldDelegate <sokudonkalitrex@gmail.com>
2023-03-14 17:54:49 -0400

    Update to WordTile
    Should now be functional enough

--- Chimera Junction/Assets/Scripts/WorldTile.cs
+++ Chimera Junction/Assets/Scripts/WorldTile.cs
@@ -7,7 +7,7 @@ public class WorldTile : MonoBehaviour
     /* Original Creator: Brian Harney
      * Creation Date: 3/13/2023
      * Additional Collaborators: 
-     * Last Updated: 3/13/2023
+     * Last Updated: 3/14/2023
      * Last Updator: Brian Harney
      * 
      * Description: Container for the information within a World Tile
@@ -15,26 +15,21 @@ public class WorldTile : MonoBehaviour
      * Usage: Instantiated for each world tile. DO not rotate a world tile; instead change their boundry types.
      */
 
-        /*
-         *                  All boundries assume that forwards is "away" from the camera, back is "towards". These objects should never be rotated.
-         */
+    /*
+     *                  All boundries assume that forwards is "away" from the camera, back is "towards". These objects should never be rotated.
+     */
 
-    public string uType;    //Upwards Boundry Type
-    public string dType;    //Downwards Boundry Type
-    public string rType;    //Right Boundry Type
-    public string lType;    //Left Boundry Type
-    public string fType;    //Forward Boundry Type
-    public string bType;    //Back Boundry Type
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<string> validNeighbors = new List<string> { "Land", "Air", "Water" }; //Valid Neighbor strings. Other strings will "work", but may cause issues
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] string uType { set { uType = value; } get { if (validNeighbors.Contains(uType)) {Debug.Log("Invalid Neighbor! At: " + this.gameObject.name);} return uType;  } }    //Upwards Boundry Type
+    [SerializeField] string dType { set { dType = value; } get { if (validNeighbors.Contains(uType)) { Debug.Log("Invalid Neighbor! At: " + this.gameObject.name); } return dType; } }     //Downwards Boundry Type
+    [SerializeField] string rType { set { rType = value; } get { if (validNeighbors.Contains(uType)) { Debug.Log("Invalid Neighbor! At: " + this.gameObject.name); } return rType; } }     //Right Boundry Type
+    [SerializeField] string lType { set { lType = value; } get { if (validNeighbors.Contains(uType)) { Debug.Log("Invalid Neighbor! At: " + this.gameObject.name); } return lType; } }     //Left Boundry Type
+    [SerializeField] string fType { set { fType = value; } get { if (validNeighbors.Contains(uType)) { Debug.Log("Invalid Neighbor! At: " + this.gameObject.name); } return fType; } }     //Forward Boundry Type
+    [SerializeField] string bType { set { bType = value; } get { if (validNeighbors.Contains(uType)) { Debug.Log("Invalid Neighbor! At: " + this.gameObject.name); } return bType; } }     //Back Boundry Type
+
+
+
+
 }
