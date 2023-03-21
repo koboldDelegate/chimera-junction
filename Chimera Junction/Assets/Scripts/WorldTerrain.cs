56cbef552b7e388eb42fc2e969e197cb4ad441de
koboldDelegate <sokudonkalitrex@gmail.com>
2023-03-21 01:46:15 -0400

    Next Step Grid Integration (still not much)

--- Chimera Junction/Assets/Scripts/WorldTerrain.cs
+++ Chimera Junction/Assets/Scripts/WorldTerrain.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class WorldTerrain : MonoBehaviour
 {
-    /*
+    /* Original Creator: Brian Harney
+     * Creation Date: 3/20/2023
+     * Additional Collaborators: 
+     * Last Updated: 3/20/2023
+     * Last Updator: Brian Harney
      * 
+     * Description: Script that creates and manages WorldTiles. Doesn't work. New lead: Grid???
      * 
-     * 
-     * */
+     * Usage: 
+     */
 
-    // Start is called before the first frame update
-    void Start()
+    static WorldTerrain instance;
+    static GameObject worldMap;
+    public GameObject mapPrefab;
+    public static Grid grid;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        if(worldMap == null)
+        {
+            worldMap =  Instantiate(mapPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            worldMap.gameObject.transform.SetParent(this.gameObject.transform);
+            worldMap.gameObject.name = "InstantiatedWorldMap";
+        }
+    }
+
+    public void GenerateWorld()     //Run this to generate the entire village map, randomized.
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void RandomizeTiles(int seed)   //Creates a 3d tilemap of random map tiles
     {
+
         
     }
+
+    void SetTile(Vector3Int pos, GameObject tile)
+    {
+
+
+    }
+
 }
